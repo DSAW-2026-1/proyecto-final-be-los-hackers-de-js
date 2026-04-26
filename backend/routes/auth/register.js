345ed42c678a0e53b526d4d32fce50570fbf4aaf
Felipe9999 <70220216+Felipe9999@users.noreply.github.com>
2026-04-26 18:56:31 -0500

    Add email input validation

--- backend/routes/auth/register.js
+++ backend/routes/auth/register.js
@@ -3,13 +3,21 @@ const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const db = require("../../dbManager")
+const { body, validationResult } = require('express-validator');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'dev_secret_change_me';
 const saltRounds = process.env.BCRYPT_SALT_ROUNDS || 10;
-router.post('/', async (req, res) => {
+router.post('/', [body('email').isEmail()], async(req, res) => {
     const errorMsg = "User, email or password is missing/malformed"
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array())
+        return res.status(400).json({ errorMsg });
+    }
     const {username, email, password} = req.body || {};
+
     if (!username || !email || !password) return res.status(400).json({error: errorMsg});
+    if (!email.endsWith("@unisabana.edu.co")) return res.status(400).json({error: errorMsg});
 
     //Check that our new user doesn't use the same username or email as someone else
     const existingUser = await db.findUser({$or: [{username: username}, {email: email}]})
