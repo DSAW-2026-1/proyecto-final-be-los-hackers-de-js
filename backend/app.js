0e415560820d90c87b50b90c3053d8a0444b3c81
Felipe9999 <70220216+Felipe9999@users.noreply.github.com>
2026-04-27 18:29:43 -0500

    Add the users API to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-//var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const loginRouter = require('./routes/auth/login');
 const adminLoginRouter = require('./routes/auth/admin/login');
 const registerRouter = require('./routes/auth/register');
@@ -28,5 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/auth/login', loginRouter);
 app.use('/api/auth/register', registerRouter);
 app.use('/api/admin/login', adminLoginRouter);
+app.use('/api/users', usersRouter)
 
 module.exports = app;
