8b7d1bb1d3cb2c12845806a2e1364119589f8eb3
Felipe9999 <70220216+Felipe9999@users.noreply.github.com>
2026-04-27 18:52:27 -0500

    Add the ability to request your own user to the users router

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const db = require("../dbManager");
+const tokenValidatorMiddleware = require('./../middleware/auth/tokenValidator');
+const userAuthMiddleware = require('./../middleware/auth/userValidator')
 
 router.get('/:id', async function (req, res, next) {
     const UID = req.params.id
@@ -16,6 +18,24 @@ router.get('/:id', async function (req, res, next) {
         reputation: user.reputation || null,
         sales: user.sales || 0
     })
+});
+router.use('/', tokenValidatorMiddleware);
+router.use('/', userAuthMiddleware);
+router.get('/', async function (req, res, next) {
+    const UID = req.token.payload.UID
+    const user = await db.findUserByUID(UID)
+
+    if (!user) return res.status(404).json({error: 'User not found'})
+    else if(user.isSuspended) return res.status(404).json({error: 'User not found'})
+    else return res.json({
+            username: user.username,
+            email: user.email,
+            isSeller: user.isSeller,
+            career: user.career || null,
+            photo: user.photo || null,
+            reputation: user.reputation || null,
+            sales: user.sales || 0
+        })
 });
 
 module.exports = router;
